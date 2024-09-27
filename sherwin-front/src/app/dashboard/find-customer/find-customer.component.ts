09a9e5326f5b356c8129acfa020e4b1f79aab9d7
ThiagoJv-pro <thijv7@gmail.com>
2024-09-26 23:04:54 -0300

    fix(#02) Ajusta endpoint e adiociona snackbar de acao

--- sherwin-front/src/app/dashboard/find-customer/find-customer.component.ts
+++ sherwin-front/src/app/dashboard/find-customer/find-customer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,12 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import { DataService } from './service/data.service';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+
 
 @Component({
   selector: 'app-find-customer',
@@ -17,6 +23,9 @@ import { CommonModule } from '@angular/common';
     MatInputModule,
     MatButtonModule,
     CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatTableModule
   ],
   templateUrl: './find-customer.component.html',
   styleUrls: ['./find-customer.component.scss']
@@ -24,9 +33,16 @@ import { CommonModule } from '@angular/common';
 export class FindCustomerComponent {
   constructor(private dataService: DataService) { }
 
+  private _snackBar = inject(MatSnackBar);
   document: string = '';
   responseData: any;
   isDocument: boolean = true; 
+
+
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
 
   toggle(): void {
     this.isDocument = !this.isDocument; // Alterna entre CNPJ e ID
@@ -44,7 +60,7 @@ export class FindCustomerComponent {
           this.responseData = response;
         },
         error => {
-          console.error('Erro ao buscar por CNPJ:', error);
+          this.openSnackBar(error.error, "fechar");
         }
       );
     } else {
@@ -54,7 +70,7 @@ export class FindCustomerComponent {
           this.responseData = response;
         },
         error => {
-          console.error('Erro ao buscar por ID:', error);
+          this.openSnackBar(error.error, "fechar");
         }
       );
     }
---
+++ sherwin_service/infrastructure/src/main/java/com/sherwin/infrastructure/customer/model/CreateCustomerResponse.java
@@ -0,0 +1,8 @@
+package com.sherwin.infrastructure.customer.model;
+
+public record CreateCustomerResponse(
+    String id,
+    String message
+) {
+    
+}
